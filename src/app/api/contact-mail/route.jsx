14d39fc6fcfe65aebbf1d484cbc3e3f8d0c3f0e9
azhargulamhusein <azhar.gulamhusein@gmail.com>
2025-01-31 18:29:31 +0400

    smtp

--- src/app/api/contact-mail/route.jsx
+++ src/app/api/contact-mail/route.jsx
@@ -14,7 +14,7 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
         host: process.env.NEXT_PUBLIC_EMAIL_HOST,
         port: 465,
-        secure: true,
+        secure: false,
         auth: {
             user: process.env.NEXT_PUBLIC_EMAIL,
             pass: process.env.APP_PASSWORD_FOR_EMAIL,
