13aea9efd2bfed55ae65271d1fa6ece72a0d78b2
azhargulamhusein <azhar.gulamhusein@gmail.com>
2025-02-27 10:10:25 +0400

    capacta

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,19 @@ import CertificationsAndMemberships from "@/components/certifications-and-member
 import Main from "@/components/hero/main";
 import OurCompany from "@/components/our-company/our-company";
 import Services from "@/components/Services/Services";
-import Milestones from "@/components/milestones/Milestones"
+import Milestones from "@/components/milestones/Milestones";
 import OurBrands from "@/components/our-brands/our-brands";
 import Cards from "@/components/Cards/Cards";
-import News from "@/components/news/News"
-import { getFooter, getHomePage, getMilestones } from "@/services"
+import News from "@/components/news/News";
+import { getFooter, getHomePage, getMilestones } from "@/services";
 
 export default async function Home() {
-  const milestones = await getMilestones()
-  const page = await getHomePage()
-  const { Side_Sticky_Links } = await getFooter()
-  
+  const [milestones, page, footer] = await Promise.all([
+    getMilestones(),
+    getHomePage(),
+    getFooter(),
+  ]);
+  const { Side_Sticky_Links } = footer;
   const {
     BrandContent,
     CertificationsMemberships,
@@ -22,18 +24,18 @@ export default async function Home() {
     Our_Company_Content,
     ServiceCard,
     news_and_events,
-  } = page
+  } = page;
 
   return (
     <>
-      <Main data={Hero} Side_Sticky_Links={Side_Sticky_Links}/>
-      <CertificationsAndMemberships data={CertificationsMemberships}/>
-      <OurCompany data={Our_Company_Content}/>
-      <Services data={ServiceCard}/>
+      <Main data={Hero} Side_Sticky_Links={Side_Sticky_Links} />
+      <CertificationsAndMemberships data={CertificationsMemberships} />
+      <OurCompany data={Our_Company_Content} />
+      <Services data={ServiceCard} />
       <Milestones data={milestones} />
-      <OurBrands data={Icons} content={BrandContent}/>
-      <Cards data={ContentCard}/>
-      <News data={news_and_events}/>
+      <OurBrands data={Icons} content={BrandContent} />
+      <Cards data={ContentCard} />
+      <News data={news_and_events} />
     </>
   );
 }
